2045d53b5caf6182f92b74586912850b7b590e36
mcardi-ux-dev <juanchodavidcardenas@outlook.com>
2022-08-25 21:09:22 -0500

    Se ha agregado las vistas de politicas y se ha cambiado la imagen de irrigacion

---
+++ TermsConditions.php
@@ -0,0 +1,87 @@
+<?php require('header.php');?>
+
+<?php require('menu.php');?>
+<section class="flat-case-details">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <h3 class="section-heading-jost-size46 fw-500 text-pri2-color center mg-bottom-50 ">Project title</h3>
+                        <p class="section-desc center mg-bottom-40">Lorem Ipsum is simply dummy text of free available in market printing typesetting industry has been industry's standard ever. Lorem Ipsum is simply dummy text of free available in market typesetting industry has been the industry's
+                            standard dummy text ever. Lorem Ipsum is simply dummy text of free available in market the printing and typesetting industry industry's standard dumm.</p>
+                        <div class="post-case-details">
+                            <img src="images/services/medium-shot-woman-plant-pot.jpg" alt="">
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="services-box mg-bottom-35 wow fadeInLeft" style="visibility: visible; animation-name: fadeInLeft;">
+                            <div class="features-box">
+                                <span class="tf-icon icon-Group-660"><span class="ripple"></span></span>
+                                <div class="content-features">
+                                    <a href="#">
+                                        <h3 class="section-heading-jost-size28 mg-bottom-8">Company Gather.</h3>
+                                    </a>
+                                    <p class="section-desc">Lorem Ipsum is simply dummy available typesetting industry been the industry standard Lorem Ipsum</p>
+                                </div>
+                            </div>
+                            <div class="post-inner-box">
+                                <img src="images/services/close-up-planting-flowers-pot.jpg" alt="images">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="services-box mg-bottom-35 wow fadeInLeft" style="visibility: visible; animation-name: fadeInLeft;">
+                            <div class="features-box">
+                                <span class="tf-icon icon-admin-sys"><span class="ripple"></span></span>
+                                <div class="content-features">
+                                    <a href="#">
+                                        <h3 class="section-heading-jost-size28 mg-bottom-8">Company Details. </h3>
+                                    </a>
+                                    <p class="section-desc">Lorem Ipsum is simply dummy text free available typesetting industry been the industry simply </p>
+                                </div>
+                            </div>
+                            <div class="post-inner-box">
+                                <img src="images/services/portrait-smiling-young-woman-holding-colorful-petunias-wooden-crate.jpg" alt="images">
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-md-12">
+                        <p class="section-desc mg-bottom-22">Lorem Ipsum is simply dummy text of free available in market printing typesetting industry has been industry's standard ever. Lorem Ipsum is simply dummy text of free available in market typesetting industry has been the industry's
+                            standard dummy text ever. Lorem Ipsum is simply dummy text of free available in market the printing and typesetting industry industry's standard dumm dummy text ever. Lorem Ipsum is simply dummy.</p>
+                        <p class="section-desc">Lorem Ipsum is simply dummy text of free available in market printing typesetting industry has been industry's standard ever. Lorem Ipsum is simply dummy text of free available in market typesetting industry has been the industry's
+                            standard.
+                        </p>
+                        <div class="themesflat-spacer clearfix" data-desktop="75" data-mobile="30" data-smobile="30" style="height:75px"></div>
+                    </div>
+                    <div class="col-md-3">
+                        <div class="author-post">
+                            <img src="images/services/portrait-woman-gardening.jpg" alt="images">
+                        </div>
+                    </div>
+                    <div class="col-md-9">
+                        <div class="author-note bd-radius-8">
+                            <h4 class="author-name section-heading-rubik-size16 fw-500 text-pri2-color">D.JHON SHIKON</h4>
+                            <p class="author-desc">Lorem Ipsum is simply dummy text of free available in market the printing and typesetting industry has been the industry's standard dummy text ever.</p>
+                        </div>
+                    </div>
+                    <div class="col-md-12">
+                        <div class="post-tags-socials">
+                            <div class="post-tags">
+                                <span class="section-heading-rubik-size18 text-pri-color mg-right-15">Tag:</span>
+                                <a class="section-heading-rubik-size12 fw-500" href="#">design</a>
+                                <a class="section-heading-rubik-size12 fw-500" href="#">ui/ux design</a>
+                                <a class="section-heading-rubik-size12 fw-500" href="#">graphics</a>
+                                <a class="section-heading-rubik-size12 fw-500" href="#">icon</a>
+                            </div>
+                            <div class="post-socials ">
+                                <a href="#" class="skype"><span class="fa fa-skype"></span></a>
+                                <a href="#" class="facebook"><span class="fa fa-facebook"></span></a>
+                                <a href="#" class="instagram"><span class="fa fa-instagram"></span></a>
+                                <a href="#" class="twitter"><span class="fa fa-twitter"></span></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+<?php require('footer.php');?>
